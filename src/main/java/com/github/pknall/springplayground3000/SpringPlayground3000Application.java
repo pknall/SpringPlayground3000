3681a2699e055ae70b1e9f89aec443ecf39c6a31
Peter Knall <peterknall@gmail.com>
2021-09-04 11:02:12 -0400

    Initial commit

---
+++ src/main/java/com/github/pknall/springplayground3000/SpringPlayground3000Application.java
@@ -0,0 +1,13 @@
+package com.github.pknall.springplayground3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlayground3000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlayground3000Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/pknall/springplayground3000/SpringPlayground3000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pknall.springplayground3000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlayground3000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
